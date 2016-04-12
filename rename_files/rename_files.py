f198cb48df5abf4614fdcf39a51519e6baba7a66
fx14 <klaus>
2016-04-12 23:14:53 +0200

    upload

---
+++ rename_files/rename_files.py
@@ -0,0 +1,15 @@
+import os
+import string
+
+def rename_files():
+    #(1) returns a list of file names
+    file_list = os.listdir("/Users/klaus.fuchs/Dropbox (Personal)/GitHub/Udacity-FullStackDeveloper/rename_files/prank")
+    print(file_list)
+    #(2) for each file, rename it
+    for file_name in file_list
+        os.rename(file_name,file_name.translate({ord(k): None for k in string.digits}))
+    print(file_list)
+rename_files()
+
+
+
